857d4a316e20c479dd3f59ee505c677bcf12abf1
ktsn <ktsn55@gmail.com>
2016-01-12 13:58:13 +0900

    Create and update tables
    - create users and groups tables
    - users and groups table is associated by groups_users table
    - add user_id and group_id column to pages table
    - create float_pages for pages behind users' desktop

---
+++ migrations/20160103014436_create_user_and_group_table.js
@@ -0,0 +1,36 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema
+    .createTable("users", (table) => {
+      table.bigIncrements("id").primary();
+      table.string("slack_user_id").unique();
+      table.string("slack_access_token");
+      table.string("name").notNullable();
+      table.string("email").unique();
+      table.string("icon_url");
+    })
+    .createTable("groups", (table) => {
+      table.bigIncrements("id").primary();
+      table.string("slack_team_id").unique();
+    })
+    .createTable("groups_users", (table) => {
+      table.bigInteger("user_id")
+        .unsigned()
+        .notNullable()
+        .references("users.id")
+        .onDelete("cascade");
+      table.bigInteger("group_id")
+        .unsigned()
+        .notNullable()
+        .references("groups.id")
+        .onDelete("cascade");
+      table.primary(["user_id", "group_id"]);
+    });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema
+    .dropTable("groups_users")
+    .dropTable("groups")
+    .dropTable("users");
+};
---
+++ migrations/20160104001042_add_group_id_to_pages_table.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.table("pages", (table) => {
+    table.bigInteger("group_id")
+      .unsigned()
+      .notNullable()
+      .references("groups.id");
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.table("pages", (table) => {
+    table.dropForeign("group_id");
+    table.dropColumn("group_id");
+  });
+};
