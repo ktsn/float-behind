911903c4511aa9a18854cd119d42469e6e0a5c8b
TETRA2000 <sagittariusm25@gmail.com>
2017-03-04 17:17:19 +0900

    wip

--- setup/init.sql
+++ setup/init.sql
@@ -2,4 +2,4 @@ create database floatbehind_db;
 
 use floatbehind_db;
 
-grant all privileges on floatbehind_db.* to floatbehind@localhost identified by 'floatbehind_pwd';
+grant all privileges on floatbehind_db.* to floatbehind@% identified by 'floatbehind_pwd';
